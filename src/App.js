c6505e216feb958fa83feba897329c1269d7538e
saquibb <saquibmsurs@gmail.com>
2020-11-10 22:51:09 +0500

    background added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import './App.css';
 
 function App() {
-  return <div>Red queen</div>;
+  return (
+    <div className='background'>
+      <img
+        src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bg_earth.jpg'
+        alt='background'
+      />
+      <img
+        className='palm1'
+        src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png'
+        alt='palm1'
+      />
+      <img
+        className='redqueen_and_alice'
+        src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png'
+        alt='redqueen_and_alice'
+      />
+      <img
+        className='bush'
+        src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png'
+        alt='bush'
+      />
+      <img
+        className='rook_upright'
+        src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png'
+        alt='rook_upright'
+      />
+      <img
+        className='r_knight'
+        src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png'
+        alt='r_knight'
+      />
+      <img
+        className='palm2'
+        src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png'
+        alt='palm2'
+      />
+    </div>
+  );
 }
 
 export default App;
